1b3c1e35f20547ccc813a2b9ae45af63bd503d91
BWash <BTWashington@student.fullsail.edu>
2021-02-20 11:46:45 -0500

    Author: BWash
    Began Work on Sample_Data_Provider
    
    Began work on the Profile_key class
    -> Profile_Key datapoint class created
       -> Profile Key field created
       -> Profile ID field created
    -> Create_Password_Key method
       -> Used Random Number Generator to create Individual Password Key
    
       ***
       began logic for encyrption of passsword

--- app/src/main/java/com/example/oasis_db/Profile_Key.java
+++ app/src/main/java/com/example/oasis_db/Profile_Key.java
@@ -1,14 +1,13 @@
 package com.example.oasis_db;
 
 import java.util.Random;
-import java.
 
 public class Profile_Key {
-	private String profile_key;
+	private Integer profile_key;
 	private String profile_id;
 
 	// Constructor for the Profile_Key
-	Profile_Key(String key,String id){
+	Profile_Key(Integer key,String id){
 		profile_key = key;
 		profile_id = id;
 	}
@@ -21,26 +20,38 @@ public class Profile_Key {
 	 * */
 	// Method to Generate Stored password -> XOR'd from Random Number generated per USER
 	// **** IDEA ****
-	public static String Create_Password_Key(String User_key, String Pf_ID){
+	public static String Create_Password_Key(Integer User_key){
 		// Generate the Random Number used for Creating individual profile_key
 		// rooted from the User defined Key
-		int TEMP = Integer.parseInt(User_key);
-		Random rndNum = new Random(TEMP);
+		Random rndNum = new Random(User_key);
 		// Return the rndNum to String format and return value to Caller
 		return rndNum.toString();
 	}
+
+
+
 
 	/**
 	 *
 	 *
 	 * */
-	// Method to Convert the Password_Key
+	// Method to use the Password_Key to convert User Defined Key to User_Password
+	public static int Convert_To_Profile_Key(String rndKey, int profile_key){
+		// Convert rndKey to a Integer
+		int intrndkey = Integer.parseInt(rndKey);
 
+		// XOR the profile key with the rndKey generated in Create_Password_Key Method
+		profile_key ^= intrndkey;
+
+		return profile_key;
+
+	}
 
 	/**
 	 *  Storing the Profile_Key in a Map Container associated with the profile_ID
 	 *  Key / Value pair -> (Profile_ID,m Profile_Key)
 	 *
+	 *  Profile_Key profile_key = new Profile_Key(rndKey,User_key);
 	 */
 
 
